170d6ef6b95f83a3d24f3f075308c13a866037fa
Marina Pimenova <mnfpma@gmail.com>
2026-09-01 16:42:39 +0300

    TI-25: AI ETL Pipeline

--- resources/liquibase/sql/_003_create_document_vector_table.sql
+++ resources/liquibase/sql/_003_create_document_vector_table.sql
@@ -1,22 +1,26 @@
+-- Metadata contract:
+-- document_id
+-- section_id
+
 CREATE TABLE IF NOT EXISTS vector_store
 (
-    id         varchar(255) PRIMARY KEY,
-    content    text,
-    metadata   json,
-    embedding  vector(1024),
-
+    id        uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    content   text,
+    metadata  jsonb,
+    embedding vector(1024),
     -- Generated columns extracted from metadata JSON
-    content_id bigint GENERATED ALWAYS AS ((metadata ->> 'id')::bigint) STORED
-    --filename   varchar(255) GENERATED ALWAYS AS ((metadata ->> 'filename')::varchar(255)) STORED
+    document_id bigint GENERATED ALWAYS AS ((metadata ->> 'document_id')::bigint) STORED,
+    section_id bigint GENERATED ALWAYS AS ((metadata ->> 'section_id')::bigint) STORED
 );
 
 -- HNSW Index for vector similarity search (using Cosine Distance)
 CREATE INDEX IF NOT EXISTS vector_store_embedding_hnsw_idx
     ON vector_store USING hnsw (embedding vector_cosine_ops);
 
--- B-Tree Indexes for efficient filtering on the generated columns
-CREATE INDEX IF NOT EXISTS idx_content_id
-    ON vector_store (content_id);
 
-/*CREATE INDEX IF NOT EXISTS idx_question_id
-    ON vector_store (filename);*/
+-- B-Tree Indexes for efficient filtering on the generated columns
+CREATE INDEX IF NOT EXISTS idx_document_id
+    ON vector_store (document_id);
+
+CREATE INDEX IF NOT EXISTS idx_section_id
+    ON vector_store (section_id);
